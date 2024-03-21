7a027344cff4c5ccea43c2acbea96ccc6c9c0ba5
AleFuzeti <104146232+AleFuzeti@users.noreply.github.com>
2024-03-20 21:26:44 -0300

    v1.0

---
+++ 1filter.py
@@ -0,0 +1,25 @@
+import csv
+
+def remover_duplicatas_e_imagens(nome_arquivo):
+    pokemons = set()  # Para manter o controle dos POKEIDs únicos encontrados
+    linhas_filtradas = []
+
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        leitor = csv.reader(arquivo, delimiter=';')
+        next(leitor)  # Pula o cabeçalho
+        for linha in leitor:
+            pokeid = int(linha[0])  # Extrai o POKEID da linha
+            if pokeid <= 151 and pokeid not in pokemons:
+                pokemons.add(pokeid)  # Adiciona o POKEID ao conjunto de pokemons
+                linha_sem_imagem = linha[:-1]  # Remove a coluna de imagem no fim
+                linhas_filtradas.append(';'.join(linha_sem_imagem))  # Adiciona a linha filtrada
+
+    # Escreve as linhas filtradas em um novo arquivo
+    with open('pok_.csv', 'w', encoding='utf-8', newline='') as arquivo_saida:
+        escritor = csv.writer(arquivo_saida, delimiter=';')
+        for linha in linhas_filtradas:
+            escritor.writerow(linha.split(';'))
+
+if __name__ == '__main__':
+    remover_duplicatas_e_imagens('pok.csv')
+    print("Linhas duplicadas e imagens removidas. Novo arquivo salvo como 'pok_csv'.")
---
+++ 4prediction.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+# Carregar os dados do arquivo CSV
+data = pd.read_csv('pok_w.csv', delimiter=';')
+
+# Função para percorrer a árvore de decisão e fazer a classificação
+def classify(data_row):
+    if data_row['Defense'] <= 56.0:
+        if data_row['HP'] <= 57.5:
+            if data_row['HP'] <= 52.5:
+                return 0
+            elif data_row['Attack'] <= 73.5:
+                if data_row['Attack'] <= 37.5:
+                    return 0
+                else:
+                    return 0
+            else:
+                return 0
+        else:
+            if data_row['Speed'] <= 85.0:
+                if data_row['HP'] <= 75.0:
+                    return 0
+                else:
+                    if data_row['Defense'] <= 32.5:
+                        return 0
+                    else:
+                        return 1
+            else:
+                return 1
+    else:
+        if data_row['HP'] <= 60.5:
+            if data_row['Speed'] <= 66.5:
+                if data_row['HP'] <= 59.5:
+                    if data_row['HP'] <= 52.5:
+                        return 0
+                    else:
+                        if data_row['Attack'] <= 73.5:
+                            if data_row['Attack'] <= 37.5:
+                                return 0
+                            else:
+                                return 0
+                        else:
+                            return 1
+                else:
+                    return 0
+            else:
+                if data_row['Sp. Def'] <= 97.5:
+                    if data_row['Defense'] <= 59.0:
+                        return 1
+                    else:
+                        return 0
+                else:
+                    return 1
+        else:
+            if data_row['HP'] <= 80.5:
+                if data_row['Defense'] <= 79.0:
+                    if data_row['HP'] <= 67.5:
+                        return 1
+                    else:
+                        if data_row['Sp. Def'] <= 72.5:
+                            if data_row['Attack'] <= 67.5:
+                                return 1
+                            else:
+                                return 0
+                        else:
+                            return 1
+                else:
+                    return 0
+            else:
+                return 1
+
+# Classificar cada linha de dados
+predictions = []
+for index, row in data.iterrows():
+    predictions.append(classify(row))
+# Adicionar as previsões ao DataFrame
+data['Prediction'] = predictions
+
+# Salvar o DataFrame com as previsões em um arquivo de texto com identação
+data[['ID', 'Win', 'Prediction']].to_csv('predictions.txt', sep='\t', index=False)
+
+print("Dados salvos em 'predictions.txt'")
